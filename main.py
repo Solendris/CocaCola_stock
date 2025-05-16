91de68ed51d3b6b54430d4b367335cd6c7f2aa9e
Solendris <139469603+Solendris@users.noreply.github.com>
2025-05-16 11:26:35 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("muhammadatiflatif/coca-cola-stock-data-over-100-years-of-trading")
+
+print("Path to dataset files:", path)
+
