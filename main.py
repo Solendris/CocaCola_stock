4645499d5a0d4fdee67949a4a304976a4ca634c6
Solendris <kornel.kolba@gmail.com>
2025-05-19 19:27:01 +0200

    database changed from SQLlite to PostgreSQL, created some graphs, updated Notes.txt

--- main.py
+++ main.py
@@ -6,38 +6,52 @@
 #
 # print("Path to dataset files:", path)
 #######################################################################################################################
-# # converting .CSV file into sqlite database
-# import sqlite3
-# import csv
-#
-# csv_file = 'KO_1919-09-06_2025-05-12.csv'
-# table_name = 'CocaCola_stock_date'
-#
-# # Otwórz plik i wczytaj dane
-# with open(csv_file, newline='', encoding='utf-8') as f:
-#     reader = list(csv.reader(f))
-#     second_row = reader[0]  # Using second line of .csv file as our colum headers
-#     data_rows = reader[2:]  # Data starts from third row
-#
-# # Prepare correct column names
-# columns = [f'"{col.strip().replace(" ", "_")}" TEXT' for col in second_row]
-# columns_str = ', '.join(columns)
-#
-# # SQLite database connection
-# conn = sqlite3.connect('project_database.db')
-# cursor = conn.cursor()
-#
-# # Table creation
-# cursor.execute(f'DROP TABLE IF EXISTS {table_name}')
-# cursor.execute(f'CREATE TABLE {table_name} ({columns_str})')
-#
-# # WData insertion
-# placeholders = ', '.join('?' * len(second_row))
-# cursor.executemany(
-#     f'INSERT INTO {table_name} VALUES ({placeholders})',
-#     data_rows
-# )
-#
-# conn.commit()
-# conn.close()
-# print(f"Table '{table_name}' has been created.")
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib
+matplotlib.use('TkAgg')
+
+
+df = pd.read_csv('cocacola_stock_data.csv')
+# print(df.head(10))
+df['date'] = pd.to_datetime(df['date'], errors='coerce')
+
+# === 6. Wykres ceny w czasie (matplotlib) ===
+plt.figure(figsize=(14, 6))
+plt.plot(df['date'], df['high'], label='high')
+plt.title("Cena akcji w czasie (Matplotlib)")
+plt.xlabel("Date")
+plt.ylabel("Cena najwyzsza")
+plt.legend()
+plt.grid(True)
+plt.tight_layout()
+plt.show()
+
+df['SMA_50'] = df['close'].rolling(window=50).mean()
+df['EMA_200'] = df['close'].ewm(span=200).mean()
+
+# === 8. Wykres średnich ruchomych ===
+plt.figure(figsize=(14, 6))
+plt.plot(df['date'], df['adj_close'], label='Adj Close', alpha=0.5)
+plt.plot(df['date'], df['SMA_50'], label='SMA 50', color='orange')
+plt.plot(df['date'], df['EMA_200'], label='EMA 200', color='green')
+plt.title("Cena i średnie ruchome")
+plt.xlabel("Data")
+plt.ylabel("Cena")
+plt.legend()
+plt.grid(True)
+plt.tight_layout()
+plt.show()
+
+
+# === 11. Korelacja między zmiennymi ===
+cols = ['open', 'high', 'low', 'close', 'adj_close', 'volume']
+correlation = df[cols].corr()
+
+plt.figure(figsize=(10, 8))
+sns.heatmap(correlation, annot=True, cmap="coolwarm", fmt=".2f")
+plt.title("Macierz korelacji")
+plt.tight_layout()
+plt.show()
